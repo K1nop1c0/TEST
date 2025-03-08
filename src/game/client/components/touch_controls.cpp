e67e1ec8770a0b7dbb3df81615d143d53a4486b7
K1nop1c0 <fkxiesp@gmail.com>
2025-03-08 16:20:51 +0800

    sb

--- src/game/client/components/touch_controls.cpp
+++ src/game/client/components/touch_controls.cpp
@@ -42,7 +42,7 @@ static constexpr int BUTTON_SIZE_SCALE = 1000000;
 static constexpr int BUTTON_SIZE_MINIMUM = 50000;
 static constexpr int BUTTON_SIZE_MAXIMUM = 500000;
 
-CTouchControls::CTouchButton CTouchControls::*SelectedButton = nullptr;
+CTouchButton *CTouchControls::SelectedButton = nullptr;
 
 /* This is required for the localization script to find the labels of the default bind buttons specified in the configuration file:
 Localizable("Move left") Localizable("Move right") Localizable("Jump") Localizable("Prev. weapon") Localizable("Next weapon")
@@ -1751,14 +1751,14 @@ void CTouchControls::RenderButtonEditor()
 		for(auto State = DeletedFingerState.begin(); State != DeletedFingerState.end(); State++)
 		{
 			bool IfNotExist = std::none_of(vTouchFingerStates.begin(), vTouchFingerStates.end(), [&State](const auto &TargetState){
-				*(State).m_Finger == TargetState.m_Finger;
+				(*State).m_Finger == TargetState.m_Finger;
 			});
 			if(IfNotExist)
 				DeletedFingerState.erase(State);
 		}
 	//Delete fingers if they are press later. So they cant be the longpress finger.
 	if(vTouchFingerStates.size() > 1)
-		std::for_each(vTouchFingerStates.begin() + 1, vTouchFingerStates.end(), [&DeletedFingerState](const auto &State){
+		std::for_each(vTouchFingerStates.begin() + 1, vTouchFingerStates.end(), [](const auto &State){
 			DeletedFingerState.push_back(State);
 		});
 
